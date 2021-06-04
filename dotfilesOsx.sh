d759547018a0f1276b85eb6b843a0e0c10b42a0a
Stephen Guo <sguo@bluecatnetworks.com>
2021-06-04 09:47:18 -0400

    Add Osx files

---
+++ dotfilesOsx.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cp -v "${HOME}/.zshrc"  \
+"${HOME}/git/dotfiles/zshrcOsx"
+
+cp -v "${HOME}/.bash_profile" \
+"${HOME}/git/dotfiles/bash_profileOsx"
+
+cp -v "${HOME}/.bashrc" \
+"${HOME}/git/dotfiles/bashrcOsx"
+
+cp -v "${HOME}/.zshenv" \
+"${HOME}/git/dotfiles/zshenvOsx"
+
+cp -v "${HOME}/.p10k.zsh" \
+"${HOME}/git/dotfiles/p10kOsx.zsh"
