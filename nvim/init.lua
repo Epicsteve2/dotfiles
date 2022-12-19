d992511f8fe9ba50f3f6cecc70ff2f5db5db903f
Stephen Guo <epicsteveoj@gmail.com>
2022-12-19 00:39:42 -0500

    I love nvim now

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,36 @@
 local cmp = require "cmp"
+
 local config = {
+  options = {
+    opt = {
+      cmdheight = 1,
+    },
+  },
+
+  lsp = {
+    skip_setup = { "rust_analyzer" }, -- will be set up by rust-tools & the typescript plugin
+    ["server-settings"] = {
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            completion = {
+              postfix = {
+                enable = false,
+              },
+            },
+          },
+        },
+      },
+    }
+  },
+
   plugins = {
     init = {
       { "farmergreg/vim-lastplace" },
-      { "tpope/vim-surround" },
+      {
+        "kylechui/nvim-surround",
+        config = function() require("nvim-surround").setup {} end,
+      },
       {
         "declancm/cinnamon.nvim",
         config = function()
@@ -11,33 +38,49 @@ local config = {
             extra_keymaps = true,
             extended_keymaps = true,
             override_keymaps = true,
-            -- max_length = -1, -- default
             always_scroll = true,
-            scroll_limit = -1,
+            scroll_limit = 250,
           }
         end,
       },
-      -- {
-      --   "pocco81/auto-save.nvim",
-      --   config = function()
-      --     require("auto-save").setup {
-      --       trigger_events = {"FocusLost", "TabLeave"}
-      --     }
-      --   end,
-      -- },
-      -- { "sainnhe/sonokai" },
+      {
+        "simrat39/rust-tools.nvim",
+        after = "mason-lspconfig.nvim", -- make sure to load after mason-lspconfig
+        config = function()
+          require("rust-tools").setup {
+            server = astronvim.lsp.server_settings "rust_analyzer", -- get the server settings and built in capabilities/on_attach
+          }
+        end,
+      },
     },
+
+    ["mason-lspconfig"] = {
+      ensure_installed = { "rust_analyzer" }, -- install rust_analyzer
+    },
+    -- {
+    --   "pocco81/auto-save.nvim",
+    --   config = function()
+    --     require("auto-save").setup {
+    --       trigger_events = {"FocusLost", "TabLeave"}
+    --     }
+    --   end,
+    -- },
+    -- { "sainnhe/sonokai" },
   },
 
   cmp.setup {
     completion = {
+      -- auto select first completion
       completeopt = 'menu,menuone,noinsert'
-    }
+    },
   },
+  cmp.setup.filetype({ 'markdown' }, {
+    enabled = false,
+  }),
 
   mappings = {
     n = {
-      -- ["<C-d>"] = { "<C-d>zz" },
+      -- Make ctrl+backspace work
       ["<C-h>"] = { "<C-W>" },
       ["<C-BS>"] = { "<C-W>" },
       ["<M-BS"] = { "<C-W>" },
@@ -56,21 +99,22 @@ local config = {
   --     sonokai_style = "shusia"
   --   }
   -- },
-  -- highlights = {
-  --   -- apply highlight group to all colorschemes (include the default_theme)
-  --   init = {
-  --     -- set the transparency for all of these highlight groups
-  --     Normal = { bg = "NONE", ctermbg = "NONE" },
-  --     NormalNC = { bg = "NONE", ctermbg = "NONE" },
-  --     CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-  --     CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-  --     CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-  --     LineNr = {},
-  --     SignColumn = {},
-  --     StatusLine = {},
-  --     NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
-  --     NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
-  --   },
-  -- },
+  highlights = {
+    -- apply highlight group to all colorschemes (include the default_theme)
+    init = {
+      -- set the transparency for all of these highlight groups
+      Normal = { bg = "NONE", ctermbg = "NONE" },
+      NormalNC = { bg = "NONE", ctermbg = "NONE" },
+      CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+      LineNr = {},
+      SignColumn = {},
+      StatusLine = {},
+      NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+      NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+    },
+  },
 }
+
 return config
