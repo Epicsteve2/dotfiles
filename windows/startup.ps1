c1ef451c63f108ebeaa0a6c053686c792bb1e312
Stephen Guo <epicsteveOJ@gmail.com>
2023-01-08 11:23:41 -0500

    windows updates

---
+++ windows/startup.ps1
@@ -0,0 +1,5 @@
+# vdesk noswitch:true on:2 run:cmd /Q /C START /MAX /D "C:\Users\Stephen\CodeMonkey\dotfiles-private" "C:\Users\Stephen\scoop\apps\neovide\current\neovide.exe"
+
+vdesk on:2 run:cmd /Q /C START /MAX /D "C:\Users\Stephen\CodeMonkey\dotfiles-private" "C:\Users\Stephen\scoop\apps\neovide\current\neovide.exe"
+
+vdesk on:2 run:wt --window 0 new-tab -p "bottom" --title "bottom"
