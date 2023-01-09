60544743549842cc30aa474aa9da18a1e4430bf7
Stephen Guo <epicsteveOJ@gmail.com>
2023-01-09 15:29:45 -0500

    windows updates

--- windows/startup.bat
+++ windows/startup.bat
@@ -1,2 +1,3 @@
+:: vdesk noswitch:true on:3 run:"C:\Program Files\Strawberry Music Player\strawberry.exe"
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
 PowerShell %USERPROFILE%\CodeMonkey\dotfiles\windows\startup.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
