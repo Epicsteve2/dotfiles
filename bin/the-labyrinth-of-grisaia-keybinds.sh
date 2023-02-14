c6be79503013fc2df53bcf39b072573bcf11d3f6
Stephen Guo <epicsteveoj@gmail.com>
2023-02-14 11:44:03 -0500

    add labyrinth of grisaia

---
+++ bin/the-labyrinth-of-grisaia-keybinds.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+if [[ -z "$1" ]]; then
+	echo >&2 'No argument supplied'
+	exit 1
+fi
+
+if [[ $(xprop -id "$(xdotool getactivewindow)" WM_CLASS | awk '{print $4}' | sed "s/\"//g") = 'steam_app_345620' ]]; then
+	IS_THE_FRUIT_OF_GRISAIA_ACTIVE=true
+else
+	IS_THE_FRUIT_OF_GRISAIA_ACTIVE=false
+fi
+
+case "$1" in
+'a')
+	if [[ $IS_THE_FRUIT_OF_GRISAIA_ACTIVE == true ]]; then
+		xdotool key a
+		sleep 0.05
+		xdotool key a
+		sleep 0.05
+		xdotool key a
+		sleep 0.05
+		xdotool key a
+		sleep 0.05
+		xdotool key a
+		sleep 0.05
+		xdotool key a
+	else
+		xdotool key a
+	fi
+	;;
+
+'s')
+	if [[ $IS_THE_FRUIT_OF_GRISAIA_ACTIVE == true ]]; then
+		xdotool key Escape
+		xdotool mousemove 612, 143
+		sleep 0.25
+		xdotool key Up
+		sleep 0.075
+		xdotool key Up
+		sleep 0.075
+		xdotool key Up
+		sleep 0.075
+		xdotool key Left
+		sleep 0.075
+		xdotool key Left
+		sleep 0.075
+		xdotool key Left
+		sleep 0.075
+		xdotool key Return
+		xdotool mousemove 638, 50
+	else
+		xdotool key s
+	fi
+	;;
+
+'l')
+	if [[ $IS_THE_FRUIT_OF_GRISAIA_ACTIVE == true ]]; then
+		xdotool key Escape
+		xdotool mousemove 612, 143
+		sleep 0.25
+		xdotool key Up
+		sleep 0.075
+		xdotool key Up
+		sleep 0.075
+		xdotool key Up
+		sleep 0.075
+		xdotool key Left
+		sleep 0.075
+		xdotool key Left
+		sleep 0.075
+		xdotool key Return
+		xdotool mousemove 638, 50
+	else
+		xdotool key l
+	fi
+	;;
+
+'space')
+	if [[ $IS_THE_FRUIT_OF_GRISAIA_ACTIVE == true ]]; then
+		xdotool key Return
+	else
+		xdotool key space
+	fi
+	;;
+
+*)
+	echo >&2 'Not a valid key...'
+	exit 1
+	;;
+esac
