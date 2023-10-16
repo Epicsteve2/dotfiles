b25b98864ca708cda6ba9f544467e57d9022bd06
Stephen Guo <epicsteveoj@gmail.com>
2023-10-16 03:34:24 +0000

    Remove unused dotfiles

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.tar.xz
+# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.tar.xz
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fc-cache -f -v
