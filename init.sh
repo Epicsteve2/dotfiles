a8f033c663fdddfc4370e4cb7d8cd5004e3ddf49
Stephen Guo <epicsteveoj@gmail.com>
2023-10-18 15:20:35 -0400

    Move nvim and update dotfiles

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
-# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.tar.xz
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.tar.xz --directory-prefix=/tmp
+tar xvf /tmp/FiraCode.tar.xz
+mkdir ~/.local/share/fonts
+mv -v /tmp/FiraCodeNerdFont* ~/.local/share/fonts
 fc-cache -f -v
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
