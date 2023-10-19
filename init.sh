fd377b6ae3398330cb1d6704e63447d6eefee989
Stephen Guo <epicsteveoj@gmail.com>
2023-10-19 12:12:35 -0400

    add fixes to terminals

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.tar.xz --directory-prefix=/tmp
-tar xvf /tmp/FiraCode.tar.xz
+mkdir /tmp/firacode/
+tar xvf /tmp/FiraCode.tar.xz --directory=/tmp/firacode/
 mkdir ~/.local/share/fonts
-mv -v /tmp/FiraCodeNerdFont* ~/.local/share/fonts
+mv -v /tmp/firacode/FiraCodeNerdFont* ~/.local/share/fonts
 fc-cache -f -v
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
