c46de3615c5bbf62bd2ddc033f5d2ade078371f2
Stephen Guo <epicsteveoj@gmail.com>
2023-10-19 23:27:10 -0400

    Update tmux and scripts

--- run_once_add_fonts_and_tmux.sh
+++ run_once_add_fonts_and_tmux.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+# add FiraCode
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.tar.xz --directory-prefix=/tmp
 mkdir /tmp/firacode/
 tar xvf /tmp/FiraCode.tar.xz --directory=/tmp/firacode/
 mkdir ~/.local/share/fonts
 mv -v /tmp/firacode/FiraCodeNerdFont* ~/.local/share/fonts
 fc-cache -f -v
+
+# tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
