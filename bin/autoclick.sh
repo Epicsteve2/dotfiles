26c20ef1c734005f79a2b3623437a9cbd11f3298
Stephen Guo <epicsteveoj@gmail.com>
2024-01-07 22:27:55 -0500

    Add scripts and update kmonad

---
+++ bin/autoclick.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# killall autoclick.sh || while xdotool mousedown 1 sleep 25 mouseup 1 sleep 25; do sleep 0.05; done
+
+echo "Starting Script"
+for i in {1..250}; do
+	xdotool mousedown 1 sleep 0.025 mouseup 1 sleep 0.025
+	sleep 0.05
+done
+echo "Finished Script"
