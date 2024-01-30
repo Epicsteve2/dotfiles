e47ad76b9b084b04211f156cf0553642de4ca8f3
Stephen Guo <epicsteveoj@gmail.com>
2024-01-29 22:23:05 -0500

    Add script to rename files

--- bin/rename-space-and-capitals.sh
+++ bin/rename-space-and-capitals.sh
@@ -2,5 +2,13 @@
 
 set -eu -o pipefail
 
-for f in *; do echo mv -v "$f" "${f// /-}"; done
-for f in *; do echo mv -v "$f" "$(echo "$f" | tr '[:upper:]' '[:lower:]')"; done
+for file in *; do
+	output="$(sed --expression 's|[^A-Za-z0-9._-]|-|g' <<<"$file" | # idk what this does tbh
+		tr '[:upper:]' '[:lower:]' |
+		sed --expression 's|_|-|g' --expression 's|\(-\)\1|\1|g')" # replace underscore with dashes
+
+	if [ "$file" != "$output" ]; then
+		echo "filename changed for "$file" in "$output""
+		# mv "${file}" ${output}
+	fi
+done
