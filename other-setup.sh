444f677961924cab197c137f237b8053717ce05d
Stephen Guo <epicsteveoj@gmail.com>
2024-02-04 19:52:30 -0500

    Remove root and other uneeded files

--- other-setup.sh
+++ other-setup.sh
@@ -2,3 +2,15 @@
 set -eux -o pipefail
 
 # add "Defaults !tty_tickets" to sudo visudo
+# '$(whoami) ALL=(ALL) NOPASSWD: ALL'
+# Add to /etc/grub.d/40_custom
+
+# menuentry "Shutdown" {
+# 	echo "System shutting down..."
+# 	halt
+# }
+#
+# menuentry "Restart" {
+# 	echo "System rebooting..."
+# 	reboot
+# }
