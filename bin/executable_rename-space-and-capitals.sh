12f8c52386eede8a439558e4f282818c1621169f
Stephen Guo <epicsteveoj@gmail.com>
2024-02-11 15:04:00 -0500

    fix rename-space-and-capitals

--- bin/executable_rename-space-and-capitals.sh
+++ bin/executable_rename-space-and-capitals.sh
@@ -2,5 +2,17 @@
 
 set -eu -o pipefail
 
-for f in *; do echo mv -v "$f" "${f// /-}"; done
-for f in *; do echo mv -v "$f" "$(echo "$f" | tr '[:upper:]' '[:lower:]')"; done
+#!/bin/bash
+
+set -eu -o pipefail
+
+for file in *; do
+	output="$(sed --expression 's|[^A-Za-z0-9._-]|-|g' <<<"$file" | # idk what this does tbh
+		tr '[:upper:]' '[:lower:]' |
+		sed --expression 's|_|-|g' --expression 's|\(-\)\1|\1|g')" # replace underscore with dashes
+
+	if [ "$file" != "$output" ]; then
+		echo "filename changed for "$file" in "$output""
+		# mv "${file}" ${output}
+	fi
+done
