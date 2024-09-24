85fdadd123f8a4db6fe6e5176e5de7f8e3c38f50
Stephen Guo <51217386+Epicsteve2@users.noreply.github.com>
2024-09-23 21:27:30 -0400

    Update chezmoi-stuff.sh

--- chezmoi-stuff.sh
+++ chezmoi-stuff.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 chezmoi init
+chezmoi --source ~/code-monkey/dotfiles init # when first starting i think
 
 chezmoi git pull -- --autostash --rebase && chezmoi diff
 chezmoi apply --dry-run --verbose
