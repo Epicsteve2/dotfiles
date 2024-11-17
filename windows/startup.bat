a9a37e6652f54a49b6098faec82239fbfb0ba10d
Stephen Guo <stephenguo14@gmail.com>
2024-11-16 20:15:04 -0500

    Clean files

--- windows/startup.bat
+++
@@ -1,3 +0,0 @@
-:: vdesk noswitch:true on:3 run:"C:\Program Files\Strawberry Music Player\strawberry.exe"
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell %USERPROFILE%\CodeMonkey\dotfiles\windows\startup.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
--- windows/startup.ps1
+++
@@ -1,5 +0,0 @@
-# vdesk noswitch:true on:2 run:cmd /Q /C START /MAX /D "C:\Users\Stephen\CodeMonkey\dotfiles-private" "C:\Users\Stephen\scoop\apps\neovide\current\neovide.exe"
-
-vdesk on:3 run:cmd /Q /C START /MAX /D "C:\Users\Stephen\CodeMonkey\dotfiles-private" "C:\Users\Stephen\scoop\apps\neovide\current\neovide.exe"
-
-vdesk on:3 run:wt --window 0 new-tab -p "bottom" --title "bottom"
