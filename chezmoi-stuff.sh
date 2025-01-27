30f5b7d9a5aa18cdb492130f0eddb37e0ce47527
Stephen Guo <stephenguo14@gmail.com>
2025-01-26 21:47:50 -0500

    Fix chezmoi-init and add fusuma

--- chezmoi-stuff.sh
+++ chezmoi-stuff.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
-chezmoi init
+chezmoi init ./
 chezmoi --source ~/code-monkey/dotfiles init # when first starting i think
 
 chezmoi git pull -- --autostash --rebase && chezmoi diff
