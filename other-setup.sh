d1550a271fca09d12bb45895c8a87c8a6233ad18
Stephen Guo <stephenguo14@gmail.com>
2025-02-01 18:30:04 -0500

    Add tmux setup script

--- other-setup.sh
+++ other-setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# in tmux, run
+tmux source ~/.config/tmux/tmux.conf
+# Press ctrl+b ctrl+shift+i
+
 # add "Defaults !tty_tickets" to sudo visudo
 # '$(whoami) ALL=(ALL) NOPASSWD: ALL'
 # Add to /etc/grub.d/40_custom
